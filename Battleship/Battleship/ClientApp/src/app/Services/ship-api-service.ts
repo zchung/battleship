8f8eee9a182cdf3b6ae0a50442a660071deeb16e
Zane <zachung85@gmail.com>
2021-02-07 00:53:53 +1100

    fixed api endpoint for ship-api-service.ts

--- Battleship/Battleship/ClientApp/src/app/Services/ship-api-service.ts
+++ Battleship/Battleship/ClientApp/src/app/Services/ship-api-service.ts
@@ -5,6 +5,7 @@ import { GameModel } from '../models/game-model';
 import { UpdateShipPositionRequest } from '../models/requests/update-ship-position-request';
 import { Result } from '../models/result-data';
 import { ShipModel } from '../models/ship-model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ import { ShipModel } from '../models/ship-model';
 export class ShipApiService {
   private baseUrl: string;
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    this.baseUrl = `${baseUrl}Ship/`;
+    this.baseUrl = `game/${environment.baseUrl}`;
   }
 
   public updateShipPosition(request: UpdateShipPositionRequest): Observable<Result<ShipModel>> {
